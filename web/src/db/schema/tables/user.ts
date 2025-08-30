2f59d00dcb39211426b6168dac0194a4d19b38f2
PedroRojasF <pedro.rojas.f24@gmail.com>
2025-08-30 09:21:05 -0500

    First commit

---
+++ web/src/db/schema/tables/user.ts
@@ -0,0 +1,20 @@
+import { boolean, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { role, genderEnum } from "..";
+
+export const user = pgTable("user", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull(),
+  username: text("username").unique(),
+  displayUsername: text("display_username"),
+  email: text("email").notNull().unique(),
+  emailVerified: boolean("emailVerified").notNull(),
+  image: text("image"),
+  role: role("role").default("member").notNull(),
+  gender: genderEnum("gender"),
+  createdAt: timestamp("createdAt").defaultNow(),
+  updatedAt: timestamp("updatedAt")
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+});
+
+export type UserType = typeof user.$inferSelect;
