0446c61356964b6415e5a2310e0e97e5e1b522d0
Don David <dadch1404@gmail.com>
2025-08-30 21:46:53 -0500

    delete image

--- apps/web/src/app/(routes)/(home)/page.tsx
+++ apps/web/src/app/(routes)/(home)/page.tsx
@@ -12,14 +12,6 @@ export default async function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center font-[family-name:var(--font-geist-sans)]">
       <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start w-full">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
         {me ? (
           <SimpleDashboard/>
         ) : (
