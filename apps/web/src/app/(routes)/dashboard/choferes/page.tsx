3a6587fc25b10a38d85060615146131e2f53c5f1
PedroRojasF <pedro.rojas.f24@gmail.com>
2025-09-13 11:42:40 -0500

    feat(choferes): implement authentication token retrieval and API call

--- apps/web/src/app/(routes)/dashboard/choferes/page.tsx
+++ apps/web/src/app/(routes)/dashboard/choferes/page.tsx
@@ -1,4 +1,20 @@
-export default function ChoferesPage() {
+import { cookies } from 'next/headers';
+
+export default async function ChoferesPage() {
+  const cookieStore = await cookies();
+  const token = cookieStore.get('better-auth.session_token');
+  if (!token) {
+    throw new Error('No auth token found');
+  }
+
+  const response = await fetch('http://localhost:4000/api/admin/trucks', {
+    headers: {
+      'Content-Type': 'application/json',
+      'Cookie': `${token.name}=${token?.value}`,
+    },
+  });
+  console.log('Response:', await response.json());
+
   return (
     <div>
       <main>
