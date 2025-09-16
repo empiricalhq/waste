0c95ec1654db82e29af524026dcab4d7f1a8e50a
David Duran <20960328+totallynotdavid@users.noreply.github.com>
2025-09-16 08:50:20 -0500

    test

--- apps/web/src/lib/auth/client.ts
+++ apps/web/src/lib/auth/client.ts
@@ -4,9 +4,9 @@ import { createAuthClient } from 'better-auth/react';
 const getBaseURL = () => {
   // we do this or else build fails, TODO
   if (typeof window === 'undefined') {
-    return 'http://localhost:3000/api/auth';
+    return 'http://localhost:3000';
   }
-  return '/api/auth';
+  return window.location.origin;
 };
 
 export const { signIn, signUp, signOut, useSession, getSession } = createAuthClient({
