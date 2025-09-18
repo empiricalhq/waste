b412b25293e408e1a2d6f6f555548d103c070f0b
David Duran <20960328+totallynotdavid@users.noreply.github.com>
2025-09-18 13:26:24 -0500

    update exports in api-contract to specify types and schemas for Route, Truck, Issue, Session, and User

--- apps/web/src/lib/api-contract/index.ts
+++ apps/web/src/lib/api-contract/index.ts
@@ -1,2 +1,4 @@
-export * from './admin';
-export * from './common';
+export type { Route, Truck } from './admin';
+export { routeSchema, routeStatusSchema, truckSchema } from './admin';
+export type { Issue, Session, User } from './common';
+export { issueSchema, sessionSchema, userSchema } from './common';
