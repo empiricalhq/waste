55def4904f29fbc9bfb71cfd177de360db0c334c
David Duran <20960328+totallynotdavid@users.noreply.github.com>
2025-09-18 13:39:18 -0500

    add ignore

--- apps/web/src/lib/api-contract/index.ts
+++ apps/web/src/lib/api-contract/index.ts
@@ -1,3 +1,4 @@
+// biome-ignore-all lint: we do need all of these exports
 export type { Route, Truck } from './admin';
 export { routeSchema, routeStatusSchema, truckSchema } from './admin';
 export type { Issue, Session, User } from './common';
