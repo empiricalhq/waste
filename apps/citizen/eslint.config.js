5d31c52f7d89c4b6835fce87f0cbae102283b213
David Duran <20960328+totallynotdavid@users.noreply.github.com>
2025-11-12 00:03:14 -0500

    regenerate eslint config

--- apps/citizen/eslint.config.js
+++ apps/citizen/eslint.config.js
@@ -1,15 +1,10 @@
-/* eslint-env node */
-import { defineConfig } from "eslint/config";
-import expoConfig from "eslint-config-expo/flat";
+// https://docs.expo.dev/guides/using-eslint/
+const { defineConfig } = require('eslint/config');
+const expoConfig = require("eslint-config-expo/flat");
 
-export default defineConfig([
+module.exports = defineConfig([
   expoConfig,
   {
     ignores: ["dist/*"],
-  },
-  {
-    rules: {
-      "react/display-name": "off",
-    },
-  },
+  }
 ]);
