033cded5c2de0ec8e10527742615f0549c0c52c8
Don David <dadch1404@gmail.com>
2025-11-12 00:26:56 -0500

    biome: format & ignore warnings

--- apps/citizen/eslint.config.js
+++ apps/citizen/eslint.config.js
@@ -1,10 +1,12 @@
+// biome-ignore-all lint/style/noCommonJs: default config set by the expo team
+
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
+const { defineConfig } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
 
 module.exports = defineConfig([
   expoConfig,
   {
     ignores: ["dist/*"],
-  }
+  },
 ]);
